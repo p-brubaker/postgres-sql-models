4c04fe4de06c05c1f280e2206a90dad0785a9473
Paul Brubaker <pbrubak@gmail.com>
2021-09-28 22:18:31 -0700

    failing test for patch a campsite by id

--- __tests__/campsites.test.js
+++ __tests__/campsites.test.js
@@ -76,6 +76,24 @@ describe('demo routes', () => {
         );
     });
 
+    it('should update a campsite by id', async () => {
+        await setup(pool);
+        await request(app)
+            .post('/api/campsites')
+            .send({ city: 'Portland', state: 'Oregon', radius: 75 });
+        const result = await request(app).patch('/api/campsites/1').send({
+            campsitename: 'Peterson Prairie Campground',
+        });
+        expect(result.body).toEqual(
+            expect.objectContaining({
+                campsitename: 'Peterson Prairie Campground',
+                id: 1,
+                lat: expect.any(Number),
+                long: expect.any(Number),
+            })
+        );
+    });
+
     afterAll(() => {
         pool.end();
         server.close();
